693ac27b3153b9ba1eb50d893d26293768e6b753
LVN <lvnambi99@gmail.com>
2021-05-17 18:54:17 +0700

    template shows features with apps in project.

---
+++ cart/cart.py
@@ -0,0 +1,70 @@
+from decimal import  Decimal
+
+from store.models import Shoe
+
+
+class Cart():
+    '''
+    A Cart class, providing some default behaviors that
+    can be inherited or overrided, as necessary.
+    '''
+
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get('skey')
+        if 'skey' not in request.session:
+            cart = self.session['skey'] = {}
+        self.cart = cart
+
+    def __add__(self, shoes, qty ):
+        '''
+        Adding and updating the users basket session data
+
+        '''
+        shoe_id = str(shoes.id)
+
+        if shoe_id in self.cart:
+            self.cart[shoe_id]['qty'] += qty
+        else:
+            self.cart[shoe_id] = {'price': str(shoes.price), 'qty': qty}
+
+        self.save()
+
+    def len(self):
+        '''
+        get the basket data and count the qty of items
+
+        '''
+        return sum(item['qty'] for item in self.session.values())
+
+    def update(self, shoe, qty):
+        '''
+        :param shoe:
+        :param qty:
+        :return:  Update values in session data
+        '''
+
+        shoe_id = shoe.id
+        if shoe_id in self.cart:
+            self.cart[shoe_id]['qty'] = qty
+        self.save()
+
+    def get_total_price(self):
+        return sum(Decimal(item('price')) * item('qty') for item in self.cart.values())
+
+    def delete(self,shoe):
+        '''
+
+        :param shoe:
+        :return: Delete item from session data
+        '''
+        shoe_id = str(shoe.id)
+
+        if shoe_id in self.cart:
+            del self.cart[shoe_id]
+            self.save()
+
+
+    def save(self):
+        self.session.modified = True
+
