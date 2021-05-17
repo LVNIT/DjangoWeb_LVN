956248a15e8661aad4fbb3755afda542df7b4e8c
LVN <lvnambi99@gmail.com>
2021-05-17 19:09:41 +0700

    Handle cart when user need buy or order.

--- cart/cart.py
+++ cart/cart.py
@@ -9,6 +9,7 @@ class Cart():
     can be inherited or overrided, as necessary.
     '''
 
+
     def __init__(self, request):
         self.session = request.session
         cart = self.session.get('skey')
@@ -16,7 +17,7 @@ class Cart():
             cart = self.session['skey'] = {}
         self.cart = cart
 
-    def __add__(self, shoes, qty ):
+    def add(self, shoes, qty ):
         '''
         Adding and updating the users basket session data
 
@@ -30,12 +31,29 @@ class Cart():
 
         self.save()
 
-    def len(self):
+    def __iter__(self):
+        """
+        Collect the shoe_id in the session data to query the database
+        :return: shoe
+        """
+        shoe_ids = self.cart.keys()
+        shoes = Shoe.objects.filter(id__in=shoe_ids)
+        cart = self.cart.copy()
+
+        for shoe in shoes:
+            cart[str(shoe.id)]['shoe'] = shoe
+
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['qty']
+            yield item
+
+    def __len__(self):
         '''
         get the basket data and count the qty of items
 
         '''
-        return sum(item['qty'] for item in self.session.values())
+        return sum(item['qty'] for item in self.cart.values())
 
     def update(self, shoe, qty):
         '''
@@ -44,13 +62,14 @@ class Cart():
         :return:  Update values in session data
         '''
 
-        shoe_id = shoe.id
+        shoe_id = str(shoe.id)
         if shoe_id in self.cart:
             self.cart[shoe_id]['qty'] = qty
         self.save()
 
+
     def get_total_price(self):
-        return sum(Decimal(item('price')) * item('qty') for item in self.cart.values())
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
 
     def delete(self,shoe):
         '''
@@ -64,6 +83,9 @@ class Cart():
             del self.cart[shoe_id]
             self.save()
 
+    def clear(self):
+        del self.session['skey']
+        self.save()
 
     def save(self):
         self.session.modified = True
